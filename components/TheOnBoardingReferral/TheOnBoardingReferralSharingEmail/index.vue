a520ce7412988dbb2632af438899c0e5211c2776
vsalahkar <vincent.salahkar@gmail.com>
2019-10-30 00:18:48 +0100

    refacto(email sharing): extract email sharing as component

---
+++ components/TheOnBoardingReferral/TheOnBoardingReferralSharingEmail/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="theOnBoardingReferralSharingEmail">
+    <p>You can also send <strong>one free month</strong> via email, separate emails with semicolon « ; »</p>
+    <textarea v-model="referralEmails" placeholder="amelie.dupont@gmail.com ; jeanmichel@gmail.com" />
+    <div class="theOnBoardingReferralSharingEmail__actions">
+      <button class="button button--primary button--hasIcon" :disabled="numberOfReferralEmails === 0">
+        <img :src="SendIcon" alt="" />
+        Send {{ numberOfReferralEmails }} email{{ numberOfReferralEmails > 1 ? 's' : '' }}
+      </button>
+      <button class="button button--secondary" @click="finishReferral">
+        Done
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+import SendIcon from '../../../static/icons/send-icon.svg'
+
+export default {
+  data: () => {
+    return {
+      referralEmails: null,
+      SendIcon
+    }
+  },
+  computed: {
+    emailsArray() {
+      if (!this.referralEmails) {
+        return []
+      }
+
+      const emailRegexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+      return this.referralEmails
+        .split(';')
+        .map((email) => email.trim())
+        .filter((email) => emailRegexp.test(email))
+    },
+    numberOfReferralEmails() {
+      return this.emailsArray.length
+    }
+  },
+  methods: {
+    finishReferral() {
+      console.info('Done')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.theOnBoardingReferralSharingEmail {
+  margin-top: 24px;
+
+  p {
+    margin-bottom: 16px;
+  }
+
+  textarea {
+    margin-bottom: 16px;
+    min-height: 128px;
+  }
+
+  &__actions {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- components/TheOnBoardingReferral/index.vue
+++ components/TheOnBoardingReferral/index.vue
@@ -13,35 +13,23 @@
         coverage! 💙
       </p>
     </header>
-    <the-on-boarding-referral-social-sharing :referral-code="user.referralCode" />
-    <section class="theOnBoardingReferral__sharingEmail ">
-      <p>You can also send <strong>one free month</strong> via email, separate emails with semicolon « ; »</p>
-      <textarea v-model="referralEmails" placeholder="amelie.dupont@gmail.com ; jeanmichel@gmail.com" />
-    </section>
-    <footer class="theOnBoardingReferral__actions">
-      <button class="button button--primary button--hasIcon" :disabled="numberOfReferralEmails === 0">
-        <img :src="SendIcon" alt="" />
-        Send {{ numberOfReferralEmails }} email{{ numberOfReferralEmails > 1 ? 's' : '' }}
-      </button>
-      <button class="button button--secondary" @click="finishReferral">
-        Done
-      </button>
-    </footer>
+    <the-on-boarding-referral-sharing-social :referral-code="user.referralCode" />
+    <the-on-boarding-referral-sharing-email />
   </article>
 </template>
 
 <script>
 import BaseUserProfilePicture from '../utils/BaseUserProfilePicture'
-
-import SendIcon from '../../static/icons/send-icon.svg'
 import ProfilePicture from '../../static/images/emma-photo.png'
 
-import TheOnBoardingReferralSocialSharing from './TheOnBoardingReferralSocialSharing'
+import TheOnBoardingReferralSharingSocial from './TheOnBoardingReferralSharingSocial'
+import TheOnBoardingReferralSharingEmail from './TheOnBoardingReferralSharingEmail'
 
 export default {
   components: {
     BaseUserProfilePicture,
-    TheOnBoardingReferralSocialSharing
+    TheOnBoardingReferralSharingSocial,
+    TheOnBoardingReferralSharingEmail
   },
   props: {
     user: {
@@ -51,31 +39,7 @@ export default {
   },
   data: () => {
     return {
-      referralEmails: null,
-      SendIcon,
       ProfilePicture
-    }
-  },
-  computed: {
-    emailsArray() {
-      if (!this.referralEmails) {
-        return []
-      }
-
-      const emailRegexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-      return this.referralEmails
-        .split(';')
-        .map((email) => email.trim())
-        .filter((email) => emailRegexp.test(email))
-    },
-    numberOfReferralEmails() {
-      return this.emailsArray.length
-    }
-  },
-  methods: {
-    finishReferral() {
-      console.info('Done')
     }
   }
 }
@@ -100,28 +64,6 @@ export default {
     position: absolute;
     top: 0;
     left: calc(-48px - 16px);
-  }
-
-  &__sharingEmail {
-    margin-top: 24px;
-
-    p {
-      margin-bottom: 16px;
-    }
-  }
-
-  &__sharingEmail {
-    textarea {
-      margin-bottom: 16px;
-      min-height: 128px;
-    }
-  }
-
-  &__actions {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-wrap: wrap;
   }
 }
 </style>
